02588775b9a35092f10f786e714bf13436656a1e
shrish <adhikari.shrish1@gmail.com>
2025-08-18 13:28:19 +0545

    Add Node.js application with Docker support
    - Created app.js to implement a simple HTTP server that responds with "Hello World!".
    - Added Dockerfile to define the environment for the Node.js application, including installation of dependencies and exposing the application port.
    - Introduced package.json to manage application metadata and dependencies.

---
+++ helm-charts/node-app/app.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const PORT = process.env.PORT || 3000;
+
+const requestHandler = (request, response) => {
+  response.end("Hello World!");
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(PORT, (err) => {
+  if (err) {
+    console.log("Error starting server", err);
+  } else {
+    console.log(`Server is running on port ${PORT}`);
+  }
+});
