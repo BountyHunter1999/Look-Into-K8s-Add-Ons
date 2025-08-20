752d7a644b9c4009a05141188c25f8cdd2ee3c38
shrish <adhikari.shrish1@gmail.com>
2025-08-20 17:21:14 +0545

    Add script to create Kustomize applications structure

---
+++ kustomize-works/create-kustomize.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+read -p "Enter the name of the kustomize app: " app_name
+
+envs=("dev" "stage" "prod")
+
+if [ -d "$app_name" ]; then
+    echo "Directory $app_name already exists"
+    exit 1
+fi
+
+if [ -z "$app_name" ]; then
+    echo "App name is required"
+    exit 1
+fi
+
+
+mkdir -p $app_name/base
+
+cat <<EOF > $app_name/base/kustomization.yaml
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+
+resources:
+  - deploy.yaml
+  - svc.yaml
+
+commonLabels:
+  company: happy_corp
+  app: $app_name
+
+commonAnnotations:
+  description: "This is a test deployment"
+EOF
+
+kubectl create deployment ${app_name}-deploy --image=nginx:latest --dry-run=client -o yaml > $app_name/base/deploy.yaml
+kubectl create service clusterip ${app_name}-svc --tcp=80:80 --dry-run=client -o yaml > $app_name/base/svc.yaml
+
+for env in "${envs[@]}"; do
+    mkdir -p $app_name/overlays/$env
+    cat <<EOF > $app_name/overlays/$env/kustomization.yaml
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+
+resources:
+  - ../../base
+
+EOF
+done
+
+echo "Kustomize app $app_name created"
