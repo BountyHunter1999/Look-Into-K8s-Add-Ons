0c19bdb3103eed73b57d2dc071321b793125617f
shrish <adhikari.shrish1@gmail.com>
2025-08-23 15:17:51 +0545

    Refactor Proxmox Terraform configuration for dynamic VM management
    - Updated main.tf to define a new resource for cloud-init VMs, allowing for dynamic configuration based on environment-specific variables.
    - Introduced a new variable `vm_configs` in vars.tf to manage VM settings such as ID, name, CPU, memory, and network configurations.
    - Enhanced VM resource definitions to include cloud-init settings for improved initialization and configuration.
    - Removed commented-out legacy VM configurations to streamline the codebase.

--- proxmox/main.tf
+++ proxmox/main.tf
@@ -2,139 +2,109 @@ locals {
   environments = ["dev", "stage", "prod"]
 }
 
+# https://registry.terraform.io/providers/Telmate/proxmox/latest/docs
 # resource "proxmox_vm_qemu" "vms" {
-#   for_each    = toset(local.environments)
-#   name        = each.value
+#   for_each = var.vm_configs
+
+#   vmid        = each.value.vm_id
+#   name        = each.value.name
 #   target_node = var.target_node
-#   # This was already created in the Proxmox UI
+
 #   clone      = var.vm_template
 #   full_clone = true
+#   bios       = "ovmf"
+#   agent      = 1
+#   scsihw     = "virtio-scsi-single"
 
-#   # VM settings:
-#   memory = each.key == "prod" ? 4096 : 2048
+#   os_type = "ubuntu"
+#   memory  = each.value.memory
 
-#   cpu {
-#     cores   = each.key == "prod" ? 2 : 1
-#     sockets = 1
-#     type    = "host"
-#   }
-
-#   vmid = 300 + index(local.environments, each.value) + 1
-
-#   # Boot order: disk first, then network
-#   boot = "order=scsi0;net0"
-
-#   # Enable QEMU guest agent
-#   agent = 1
-
-#   # Enable serial console
-#   serial {
-#     type = "socket"
-#     id   = 0
-#   }
-
-
+#   vm_state = each.value.vm_state
 
 #   disks {
 #     scsi {
 #       scsi0 {
 #         disk {
-#           backup             = true
-#           cache              = "none"
-#           discard            = true
-#           emulatessd         = true
-#           iothread           = true
-#           mbps_r_burst       = 0.0
-#           mbps_r_concurrent  = 0.0
-#           mbps_wr_burst      = 0.0
-#           mbps_wr_concurrent = 0.0
-#           replicate          = true
-#           size               = 32
-#           storage            = "local-lvm"
+#           size    = each.value.disk_size
+#           storage = "local-lvm"
+#           format  = "qcow2"
 #         }
 #       }
 #     }
 #   }
 
-#   bios = "seabios"
-
-
-#   network {
-#     id        = 0
-#     bridge    = "vmbr0"
-#     firewall  = false
-#     link_down = false
-#     model     = "virtio"
+#   cpu {
+#     cores   = each.value.cores
+#     sockets = 1
+#     type    = "host"
 #   }
-
 # }
 
-resource "proxmox_vm_qemu" "control_server" {
-  name        = "control-server"
+
+resource "proxmox_vm_qemu" "cloudinit-vms" {
+  for_each = var.vm_configs
+
+  vmid        = each.value.vm_id
+  name        = each.value.name
   target_node = var.target_node
-  # This was already created in the Proxmox UI
+
   clone      = var.vm_template
   full_clone = true
+  bios       = "ovmf"
+  agent      = 1
+  scsihw     = "virtio-scsi-single"
 
-  # VM settings:
-  memory = 4096
+  os_type = "cloud-init"
+  memory  = each.value.memory
 
-  vmid = 300
+  vm_state = each.value.vm_state
+  onboot   = each.value.onboot
+  startup  = each.value.startup
 
-
-  cpu {
-    cores   = 4
-    sockets = 1
-    type    = "host"
-  }
-
-  # Boot order: disk first, then network
-  boot = "order=scsi0;net0"
-
-  # Enable QEMU guest agent
-  #   agent = 1
-
-  bios = "seabios"
-
-
-  serial {
-    type = "socket"
-    id   = 0
-  }
+  # cloud init configuration
+  ipconfig0  = each.value.ipconfig
+  skip_ipv6  = true
+  ciuser     = each.value.ciuser
+  cipassword = each.value.cipassword
+  sshkeys    = each.value.sshkeys
 
   disks {
     scsi {
       scsi0 {
         disk {
-          backup             = true
-          cache              = "none"
-          discard            = true
-          emulatessd         = true
-          iothread           = true
-          mbps_r_burst       = 0.0
-          mbps_r_concurrent  = 0.0
-          mbps_wr_burst      = 0.0
-          mbps_wr_concurrent = 0.0
-          replicate          = true
-          size               = 80
-          storage            = "local-lvm"
+          size      = each.value.disk_size
+          storage   = "local-lvm"
+          replicate = true
+        }
+      }
+    }
+
+    ide {
+      ide0 {
+        cdrom {
+          file = "cloudinit.iso"
         }
       }
     }
   }
 
-  scsihw = "virtio-scsi-single"
+  cpu {
+    cores   = each.value.cores
+    sockets = 1
+    type    = "host"
+  }
+
+  serial {
+    id   = 0
+    type = "socket"
+  }
 
   network {
     id       = 0
-    bridge   = "vmbr0"
     model    = "virtio"
-    firewall = false
+    bridge   = each.value.bridge
+    firewall = true
+    tag      = each.value.network_tag
   }
-
-  hotplug = "network,disk,usb"
-
-  kvm = true
-  #   vm_state = "start_after_create"
 }
 
--- proxmox/vars.tf
+++ proxmox/vars.tf
@@ -22,3 +22,88 @@ variable "target_node" {
   default = "pve"
 }
 
+variable "vm_configs" {
+  type = map(object({
+    vm_id       = number
+    name        = string
+    cores       = number
+    memory      = number
+    vm_state    = string
+    bridge      = string
+    disk_size   = string
+    onboot      = bool
+    startup     = bool
+    network_tag = number
+    ipconfig    = string
+    ciuser      = string
+    cipassword  = string
+    sshkeys     = string
+  }))
+
+  default = {
+    "control_server" = {
+      vm_id       = 301
+      name        = "controlServer",
+      cores       = 4,
+      memory      = 8192,
+      vm_state    = "running",
+      bridge      = "vmbr0",
+      disk_size   = "60G",
+      onboot      = true,
+      startup     = true,
+      network_tag = 301,
+      ipconfig    = "ip=dhcp",
+      ciuser      = "k8sUser"
+      cipassword  = "k8sUserpass"
+      sshkeys     = "some_public_key"
+    }
+    "prod" = {
+      vm_id       = 302
+      name        = "prod",
+      cores       = 2,
+      memory      = 4096,
+      vm_state    = "running",
+      bridge      = "vmbr0",
+      disk_size   = "20G",
+      onboot      = true,
+      startup     = true,
+      network_tag = 302,
+      ipconfig    = "ip=dhcp",
+      ciuser      = "k8sUser"
+      cipassword  = "k8sUserpass"
+      sshkeys     = "some_public_key"
+    },
+    "dev" = {
+      vm_id       = 303
+      name        = "dev",
+      cores       = 1,
+      memory      = 2048,
+      vm_state    = "running",
+      bridge      = "vmbr0",
+      disk_size   = "15G",
+      onboot      = true,
+      startup     = true,
+      network_tag = 303,
+      ipconfig    = "ip=dhcp",
+      ciuser      = "k8sUser"
+      cipassword  = "k8sUserpass"
+      sshkeys     = "some_public_key"
+    },
+    "stage" = {
+      vm_id       = 304
+      name        = "stage",
+      cores       = 1,
+      memory      = 2048,
+      vm_state    = "running",
+      bridge      = "vmbr0",
+      disk_size   = "15G",
+      onboot      = true,
+      startup     = true,
+      network_tag = 304,
+      ipconfig    = "ip=dhcp",
+      ciuser      = "k8sUser"
+      cipassword  = "k8sUserpass"
+      sshkeys     = "some_public_key"
+    },
+  }
+}
